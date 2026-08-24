e73d30ca6955ae73ffbda2334537ceb1ab5cdcd0
Hanjisang <zhangzhengyu997@gmail.com>
2026-08-24 17:50:55 +0800

    fix: export execution integrity service

--- apps/api/src/project-plans/project-plans.module.ts
+++ apps/api/src/project-plans/project-plans.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { DocumentsModule } from '../documents/documents.module';
+import { ExecutionIntegrityService } from './execution-integrity.service';
 import { ProjectPlansController } from './project-plans.controller';
 import { ProjectPlansService } from './project-plans.service';
 import { ProgressService } from './progress.service';
+
 @Module({
   imports: [AuthModule, DocumentsModule],
   controllers: [ProjectPlansController],
-  providers: [ProjectPlansService, ProgressService],
-  exports: [ProjectPlansService, ProgressService],
+  providers: [ProjectPlansService, ProgressService, ExecutionIntegrityService],
+  exports: [ProjectPlansService, ProgressService, ExecutionIntegrityService],
 })
 export class ProjectPlansModule {}
